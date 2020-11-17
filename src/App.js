ec63df021622ab9ae62c353b186d2087151830f6
chayma ghazouani <ghazouanic>
2020-11-17 15:19:31 +0100

    up date state

--- src/App.js
+++ src/App.js
@@ -1,59 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 class App extends React.Component {
 
-  constructor(props){
-    super(props)
-    this.state ={
+  state ={
+    imgSrc  :  <img src="/toff.jpg" /> ,
+
+
       fullName :  " chayma  ghazouani " , 
       bio : "bio" , 
       profession : "devlopper ",
-      isChecked : false ,
+      isChecked : true ,
       digit:0,
+
     };  
-    this.handelClick =this.handelClick.bind(this);
-  }
-  handelClick(){
-    this.setState(state=>({
-      isChecked :!state.isChecked 
-    }))
-  }
-  componentDidMount =()=>{
-    setInterval =(()=>{
-      this.setState(prevState =>({
-        digit: prevState.digit +1 
-      }));
-    }, 1000);
 
-     }; 
-  isDisplayed() {
-
-    var x = document.getElementById("myDIV");
-      x.style.display = "block";
-    }
-    noDisplayed() {
-
-      var x = document.getElementById("myDIV");
-        x.style.display = "none";
-      }
-
-
+    componentDidMount = () =>{
+      setInterval(()=>{
+        this.setState(prevState=>({digit : prevState.digit +1 ,}));
+      },1000);
+    } ;
   
+     
+
+    handelClick =()=>this.setState({isChecked : !this.state.isChecked})
   render(){
      return (
      <React.Fragment>
     <button onClick={this.handelClick }> click me </button>
-    {this.state.isChecked ? this.isDisplayed() :this.noDisplayed()}
-     <div id="myDIV" >
+     { this.state.isChecked && ( <div id="myDIV" >
      <h1> {this.state.fullName}</h1>
      <h1> {this.state.bio}</h1>
      <h1> {this.state.imgSrc}</h1>
-     <h1> {this.state.fullName}</h1>
+     <h1> {this.state.profession}</h1>
+     </div>)}
+
      <span> counter : {this.state.digit}</span>
-     </div>
 
      </React.Fragment>
+
+          
+
+
+            
+
   );
 }
 
